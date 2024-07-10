4066a505ba684ce8584e26048a655a3fec7282de
Nimesh Jethva <nimesh.jethva@wonderbiz.in>
2024-07-10 11:05:35 +0530

    first commit

---
+++ KrishnaMurthyNo/KrishnaMurthyNo/KrishnaMurthyNo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KrishnaMurthyNo
+{
+    internal class KrishnaMurthyNo
+    {
+        public static void Main(string[] args)
+        {
+            int n = 145;
+            int temp = n;
+            int sum = 0;
+            while (temp != 0) {
+                int res = temp % 10;
+                sum += Fact(res);
+                temp /= 10;
+            }
+            if(n == sum)
+            {
+                Console.WriteLine("Krishna Murthy Number");
+            } else {
+                Console.WriteLine("Not a Krishna Murthy Number");
+            }
+        }
+        
+        public static int Fact(int n)
+        {
+            int fact = 1;
+            for (int i = 1; i <= n; i++) { 
+                fact = fact * i;
+                
+            }
+            return fact;
+        }
+    }
+}
---
+++ ValidAnagram/ValidAnagram/ValidAnagram.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ValidAnagram
+{
+    internal class ValidAnagram
+    {
+        public static void Main(String[] args)
+        {
+            string str1 = Console.ReadLine();
+            string str2 = Console.ReadLine();
+
+            if (IsAnagram(str1, str2))
+            {
+                Console.WriteLine("Valid Anagram");
+            } else
+            {
+                Console.WriteLine("Not Valid Anagram");
+            }
+        }
+
+
+        public static bool IsAnagram(string s, string t)
+        {
+            if (s.Length != t.Length) return false;
+
+            Dictionary<char, int> map = new Dictionary<char, int>();
+
+            string s1 = new string(s.OrderBy(c => c).ToArray();
+            string s2 = new string(t.OrderBy(c => c).ToArray());
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s1[i] != s2[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
