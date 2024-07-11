c26a4328438c75bf554e464ea5d214ccf3358b79
Nimesh Jethva <nimesh.jethva@wonderbiz.in>
2024-07-11 13:06:42 +0530

    second commit

---
+++ NewBottles/NewBottles/NewBottles.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewBottles
+{
+    public class NewBottles
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Number Bottle : ");
+            int numBottle = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Number Exchange : ");
+            int numExchange = Convert.ToInt32(Console.ReadLine());
+            int tot = Bottles(numBottle, numExchange);
+            Console.WriteLine("Total Water Bottles : " + tot);
+        }
+
+        public static int Bottles(int numBottle, int numExchange)
+        {
+            int tot = numBottle; 
+            int empty = numBottle;
+
+            while (empty >= numExchange)
+            {
+                
+                int newBottles = empty / numExchange;
+                empty = empty % numExchange;
+                tot += newBottles;
+                empty += newBottles;
+            }
+
+            return tot;
+        }
+    }
+}
---
+++ SelectionSort/SelectionSort/SelectionSort.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionSort
+{
+    internal class SelectionSort
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the size of array : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Unsorted array : ");
+            int[] arr = new int[n];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int temp;
+            for (int i = 0; i < arr.Length; i++) {
+                for (int j = i + 1; j < arr.Length; j++) { 
+                    if(arr[j] < arr[i])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                    
+                }
+            }
+            Console.WriteLine("Sorted array : ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+
+        }
+    }
+}
